e9a90a07edae4ed15f8fb105b84412babc877925
cyphersprojekt <89570332+cyphersprojekt@users.noreply.github.com>
2022-04-29 16:35:25 -0300

    Update CheckVM.py

--- CheckVM.py
+++ CheckVM.py
@@ -14,15 +14,15 @@ def check_vm(ip, id):
         logging.critical(str(datetime.datetime.now()) + " VM {id} at {ip} is down".format(ip=ip, id=id))
 
         try:
-            logging.info(str(datetime.datetime.now() + "Attempting to unlock VM {id} at {ip}".format(ip=ip, id=id)))
+            logging.info(str(datetime.datetime.now()) + "Attempting to unlock VM {id} at {ip}".format(ip=ip, id=id))
             subprocess.run(['qm', 'unlock', id], capture_output=True)
-            logging.info(str(datetime.datetime.now() + "Unlocked VM {id} at {ip}, now attempting to stop it".format(ip=ip, id=id)))
+            logging.info(str(datetime.datetime.now()) + "Unlocked VM {id} at {ip}, now attempting to stop it".format(ip=ip, id=id))
             subprocess.run(['qm', 'stop', id], capture_output=True)
-            logging.info(str(datetime.datetime.now() + "Stopped VM {id} at {ip}, now attempting to start it".format(ip=ip, id=id)))
+            logging.info(str(datetime.datetime.now()) + "Stopped VM {id} at {ip}, now attempting to start it".format(ip=ip, id=id))
             subprocess.run(['qm', 'start', id], capture_output=True)
 
         except:
-            logging.critical(str(datetime.datetime.now() + "Failed to unlock and start VM {id} at {ip}".format(ip=ip, id=id)))
+            logging.critical(str(datetime.datetime.now()) + "Failed to unlock and start VM {id} at {ip}".format(ip=ip, id=id))
 
 logging.info(str(datetime.datetime.now()) + "CheckVM.py started")
 logging.info(str(datetime.datetime.now()) + "Pinging VM 106")
