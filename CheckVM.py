cbddb89dec36aad373c4a6cc7b7b7c1a8a212e3a
Ignacio Muñoz <soporte@dynagroup.com.ar>
2022-04-29 15:21:39 -0300

    create CheckVM.py

---
+++ CheckVM.py
@@ -0,0 +1,27 @@
+import ping3, datetime, subprocess, logging
+
+logging.basicConfig(filename='/var/log/CheckVM.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s')
+
+def check_vm(ip, id):
+
+    if ping3.ping(ip):
+        print(str(datetime.datetime.now()) + " VM {id} at {ip} is up".format(ip=ip, id=id))
+
+    else:
+        print(str(datetime.datetime.now()) + " VM {id} at {ip} is down".format(ip=ip, id=id))
+        logging.critical(str(datetime.datetime.now()) + " VM {id} at {ip} is down".format(ip=ip, id=id))
+
+        try:
+            logging.info(str(datetime.datetime.now() + "Attempting to unlock VM {id} at {ip}".format(ip=ip, id=id)))
+            subprocess.run(['qm', 'unlock', id], capture_output=True)
+            logging.info(str(datetime.datetime.now() + "Unlocked VM {id} at {ip}, now attempting to stop it".format(ip=ip, id=id)))
+            subprocess.run(['qm', 'stop', id], capture_output=True)
+            logging.info(str(datetime.datetime.now() + "Stopped VM {id} at {ip}, now attempting to start it".format(ip=ip, id=id)))
+            subprocess.run(['qm', 'start', id], capture_output=True)
+        
+        except:
+            logging.critical(str(datetime.datetime.now() + "Failed to unlock and start VM {id} at {ip}".format(ip=ip, id=id)))
+
+logging.info(str(datetime.datetime.now() + "CheckVM.py started"))
+logging.info(str(datetime.datetime.now() + "Pinging VM 106"))
+check_vm('192.168.18.7', '106')
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Proxmox_CheckVM
+
+### Made to run from within Proxmox.
+### Pings your desired IP. If it succeeds, it does nothing. If it fails, it attempts to restart the VM.
+
+## Requires:
+
+* python3
+* python3-pip (to install ping3)
+* ping3
