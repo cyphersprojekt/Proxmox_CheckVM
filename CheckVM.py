2cc74ade860a130be264504b38024aedc493d5ce
cyphersprojekt <munoz.ignacioagustin@gmail.com>
2022-05-02 19:00:12 -0300

    TUI that still doesn't work

--- CheckVM.py
+++ CheckVM.py
@@ -3,7 +3,7 @@
 PATH_QM_BIN='/usr/bin/qm'
 PATH_LOG='/var/log/CheckVM.log'
 
-import ping3, datetime, subprocess, logging
+import ping3, datetime, subprocess, logging, argparse, sys
 
 logging.basicConfig(filename=PATH_LOG,
                     filemode='a', 
@@ -30,14 +30,24 @@ def check_vm(ip, id):
         try:
             info(f"Attempting to unlock {vm}")
             run('unlock', id)
+        except:
+            warn(f"Failed to unlock {vm}")
+        try:
             info(f"Unlocked {vm}, now attempting to stop it")
             run('stop', id)
+        except:
+            warn(f"Failed to stop {vm}")
+        try:
             info(f"Stopped {vm}, now attempting to start it")
             run('start', id)
         except:
-            warn(f"Failed to unlock and start {vm}")
+            warn(f"Failed to start {vm}")
 
 if __name__ == '__main__':
     info("CheckVM.py started")
-    info("Pinging VM 106")
-    check_vm('192.168.18.7', '106')
+    parser = argparse.ArgumentParser(description='Check VM status')
+    parser.add_argument('-i', '--ip', type=str, help='IP address of VM')
+    parser.add_argument('-id', '--id', type=str, help='ID of VM')
+    args = parser.parse_args(sys.argv[1:])
+    info("Pinging VM {id} at {ip}")
+    check_vm(args.ip, args.id)
